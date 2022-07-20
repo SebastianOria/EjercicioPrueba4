22684250f23a084f175c148e0e1fc87560967288
SebastianOria <sebastian22oria@gmail.com>
2022-07-20 13:42:47 -0400

    aVANCES

---
+++ Modelos/DAL/ILecturasDAL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos.DAL
+{
+    public interface ILecturasDAL
+    {
+        List<Lectura> Filtrar(int medidor);
+        void AgregarLectura(Lectura lectura);
+
+        List<Lectura> ObtenerLecturas();
+
+    }
+}
---
+++ Modelos/DAL/IMedidorDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos.DAL
+{
+    public interface IMedidorDAL
+    {
+        List<Medidor> ObtenerMedidores();
+
+        Medidor Obtener(string tipo);
+
+        void AgregarMedidor(Medidor medidor);
+
+      
+    }
+}
